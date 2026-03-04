73efa27e405b08199a5ba138b66deea8f3d564a0
Rod Gaitan <rodgaitan01@gmail.com>
2026-03-04 02:51:40 -0500

    commit context FavoritesContext

---
+++ src/context/FavoritesContext.jsx
@@ -0,0 +1,42 @@
+import { createContext, useContext, useState } from 'react' //  Importing 3 React hooks
+                                                            // useContext = reads from the context inside any component
+                                                            // useState = imported for context ONLY, otherwise no need since useLocalStorage handles state.
+import useLocalStorage from '../hooks/useLocalStorage' // ..and one of the custom hooks
+
+const FavoritesContext = createContext()   // defining the context 'FavoritesContext', where our favs will be stored
+
+export function FavoritesProvider({ children }) { // defining provider component 'FavoritesProvider', which is the wrapper that makes 'favorites' data available to every component inside itself
+                                                  // 'children' parameter indicates whatever components are wrapped inside this have access to component 'FavoritesProvider'
+  const [favorites, setFavorites] = useLocalStorage('favorites', []) // here the 'useLocalStorage' hook to:
+                                                                    //  - store the list favorite saved recipes in 'favorites'
+                                                                    // - update that list uisng the 'setFavorites'
+                                                                   // - 'favorites' =  the key label given to the localStorage entry, that starts as an empty array ('[]') if nothing is saved yet
+
+  const addFavorite = (recipe) => { // Function to add a recipe to favorites: 
+                                    // it takes 1 param  
+    setFavorites([...favorites, recipe])  /// and then calls the 'setFavorites()' function to pass it:
+                                            // the spread operator '...favorites'
+                                            // and and then add the 'recipe' value to update the copy of 'favorites' array
+  }
+
+  const removeFavorite = (recipeId) => { //  Function to remove a recipe from favorites:
+                                        // it takes 1 param  
+    setFavorites(favorites.filter(recipe => recipe.idMeal !== recipeId)) // '.filter()' = loops through every favorite item and 
+                                                                            // keeps only the ones that DO NOT match the 'recipeId' THAT WE WANT TO REMOVE.
+  }
+
+  const isFavorite = (recipeId) => {  // Function to check if a recipe is ALREADY a favorite:
+    return favorites.some(recipe => recipe.idMeal === recipeId) // '.some()' = loops through the list of favorite items and returns true if at least one item matches
+                                                                // This used to decide whether to show "Add to Favorites" or "Remove from Favorites" button
+  }
+
+  return (
+    <FavoritesContext.Provider value={{ favorites, addFavorite, removeFavorite, isFavorite }}> {/* the values/functions here are being packaged and shared with any component that will be added in the 'main.jsx' file*/}
+      {children}
+    </FavoritesContext.Provider>
+  )
+}
+
+export function useFavorites() {
+  return useContext(FavoritesContext)
+}
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,16 @@ import App from './App.jsx'  // this imports 'App' component, which holds all ro
 import { BrowserRouter } from 'react-router-dom'  // imports the 'routing container' from 'React Router'. 
                                                   //Without this app wrapper, page navigation (/home, /favorites, etc.) would fail.
 
+import { FavoritesProvider } from './context/FavoritesContext'  // *1) imporitng 'FavoritesProvider' so that ALL the componenents inside the 'App' will have access to values/functions:
+                                                                // favorites, addFavorite, removeFavorite, isFavorite
+
 createRoot(document.getElementById('root')).render(  //defines the specific html element in the 'index.html' file that React will live in. ITC the 'root' 
                                                       //See "nesting dolls" below:
   <StrictMode>  {/*development helper tool */}
-        <BrowserRouter>   {/* allows routing to happen*/}
+        <BrowserRouter>   {/* allows routing to happen everrwhere*/}
+          <FavoritesProvider>                               {/*2) wrapping App with this so that the favorites work everywhere */}
           <App />       {/* the applicaiton itself */}
+          </FavoritesProvider>
         </BrowserRouter>
   </StrictMode>,
 )
