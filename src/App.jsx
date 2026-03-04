59e1adb417d9bfc8419dd9bacce3bd933a4f98be
Rod Gaitan <rodgaitan01@gmail.com>
2026-03-03 22:40:48 -0500

    commit project set up; file structure; local/remote repos

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,35 @@
+
+import { Routes, Route } from 'react-router-dom' // components from the 'react-router-dom library', which is a separate third-party package.
+                                                // 'Route'  defines each individual page and its URL path, see below in JSX:
+                                                // the container that wraps all the routes, see below in JSX:
+// importing all required PAGES & navbar                                                
+import Navbar from './components/Navbar'        
+import Home from './pages/Home'
+import CategoryPage from './pages/CategoryPage'
+import RecipeDetail from './pages/RecipeDetail'
+import Favorites from './pages/Favorites'
+import SearchResults from './pages/SearchResults'
+
+function App() {   //defines the main App component, whose purpose in this case is to return JSX of all the page routes 
+  return (
+    <>  {/*fragment marks the beginning of SINGLE parent elemment */}
+      <Navbar />  {/* Navbar elements outside Routes in Global scope so that it renders on every single page. Making it possible for search bar to always be visible. */}
+      <Routes>  {/* Each line below maps a a URL to a page, where 'path' = the URL & 'element' = the page component to render: */}
+        <Route path="/" element={<Home />} /> 
+        <Route path="/category/:categoryName" element={<CategoryPage />} /> {/* ':categoryName' is dynamic variable that changes based on what the user clicks.*/}
+        <Route path="/recipe/:recipeId" element={<RecipeDetail />} />    {/* ':recipeId' is dynamic variable that changes based on what the user clicks.*/}
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/search" element={<SearchResults />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App  // this line exports the 'App' component so that 'main.jsx' can import and use it.
+
+
+
+/*
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -33,3 +65,5 @@ function App() {
 }
 
 export default App
+
+*/
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react'  // a helpful debugging tool.
+import { createRoot } from 'react-dom/client' // connects  React app to the HTML page.
+import './index.css'  // Imports all CSS styles that need to be applied globally in the app.
+import App from './App.jsx'  // this imports 'App' component, which holds all routes.
+import { BrowserRouter } from 'react-router-dom'  // imports the 'routing container' from 'React Router'. 
+                                                  //Without this app wrapper, page navigation (/home, /favorites, etc.) would fail.
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
+createRoot(document.getElementById('root')).render(  //defines the specific html element in the 'index.html' file that React will live in. ITC the 'root' 
+                                                      //See "nesting dolls" below:
+  <StrictMode>  {/*development helper tool */}
+        <BrowserRouter>   {/* allows routing to happen*/}
+          <App />       {/* the applicaiton itself */}
+        </BrowserRouter>
   </StrictMode>,
 )
